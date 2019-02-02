f1ff9a1f9e175d205a815cda477ddebdb64853ed
YaroslavW <doctor355@yahoo.com>
2019-02-02 15:18:03 +0300

    made component todos and passed to it the status value of the parent component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Study React',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Create an application',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Find a job React - developer',
+        completed: false
+      },
+    ]
+  }
   render() {
+    // console.log(this.state.todos)
     return (
       <div className="App">
-        App
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        console.log(this.props.todos)
+      return (
+        <div>
+          Todos
+        </div>
+      );
+    }
+  }
+  
+  export default Todos;
