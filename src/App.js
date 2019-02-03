0078379d46bc94e42b76562d88537f6239569caf
YaroslavW <doctor355@yahoo.com>
2019-02-03 19:13:24 +0300

    add style as function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Create an application',
-        completed: false
+        completed: true
       },
       {
         id: 3,
