2e80c046635eeb9430360187a7e378cf3fd1d90d
YaroslavW <doctor355@yahoo.com>
2019-02-03 19:59:52 +0300

    transferred the function to the parent component and passed it in properties

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,14 +10,12 @@ export class TodoItem extends Component {
       textDecoration: this.props.todo.completed ? 'line-through' : 'none'
     }
   }
-  markComplete = (e) => {
-    console.log( this.props)
-  }
+
   render() {
     return (
       <div style={ this.getStyle() }>
         <p>
-          <input type="checkbox" onChange={ this.markComplete } /> {' '}    
+          <input type="checkbox" onChange={ this.props.markComplete } /> {' '}    
           { this.props.todo.title }
         </p>
       </div>
