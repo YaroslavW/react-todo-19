f7a97b6022f48d1417c3912df0ce99514852d32e
YaroslavW <doctor355@yahoo.com>
2019-02-04 16:03:27 +0300

    moved function markComplete in file App.js

--- src/App.js
+++ src/App.js
@@ -22,11 +22,15 @@ class App extends Component {
       },
     ]
   }
+  markComplete = (e) => {
+    console.log( 'From App.js')
+  }
+
   render() {
    
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
